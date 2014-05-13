bfbe67f927aa223a6046b77380aebf98f16f8e7e
Adriano Margarin <adrcxs@gmail.com>
2014-05-13 18:06:43 -0300

    Setup

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@
 import os
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
-    install_requires=required,
+    install_requires=REQUIREMENTS,
     name = "cpf_cnpj",
-    version = '0.1.0',
+    version = '0.1.3',
     description = "Validador de CPF e CNPJ para Python",
     license = "MIT",
     author = "Adriano Margarin",
